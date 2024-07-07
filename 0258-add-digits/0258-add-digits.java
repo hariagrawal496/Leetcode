23d9a0b5964dcb3dfe38ce6b013ed5f088721bd7
hariagrawal496 <hariagrawal.496@gmail.com>
2024-07-07 15:43:20 +0530

    Time: 1 ms (46.37%), Space: 41.4 MB (5.11%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int addDigits(int num) 
+    {
+        int temp = num ;
+        int sum = 0 ;
+
+        while( temp != 0 )
+        {
+            sum += temp % 10  ;
+            temp = temp/10 ;
+
+            if(temp == 0)
+            {
+                if(sum > 9)
+                {
+                    temp = sum ;
+                    sum = 0 ;
+                }
+            }
+        }
+        return sum ;
+    }
+}
