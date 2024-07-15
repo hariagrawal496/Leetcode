96d9dd14ffa58e25b02eb9061e701175dbffd22f
hariagrawal496 <hariagrawal.496@gmail.com>
2024-07-15 14:35:59 +0530

    Time: 407 ms (5.17%), Space: 45.6 MB (5.95%) - LeetHub

--- 0043-multiply-strings/0043-multiply-strings.java
+++ 0043-multiply-strings/0043-multiply-strings.java
@@ -1,43 +1,39 @@
 class Solution {
     public String multiply(String num1, String num2) 
     {
-        if( num1.charAt(0) == '0' || num2.charAt(0) == '0' )
+        if(num1.charAt(0) == '0' || num2.charAt(0) == '0')
         {
             return "0" ;
         }
 
-        int n = num1.length() ;
-        int m = num2.length() ;
-
+        int m = num1.length() ;
+        int n = num2.length() ;
         int[] arr = new int[m+n] ;
-
-        for(int i=n-1 ; i>=0 ; i--)
+        
+        for(int i=m-1; i>=0; i--)
         {
-            for(int j=m-1 ; j>=0 ; j--)
+            for(int j=n-1 ; j>=0; j--)
             {
-                 arr[i+j+1] += (num1.charAt(i)-'0') * (num2.charAt(j)-'0') ; 
-                 arr[i+j] += arr[i+j+1]/10 ;
-                 arr[i+j+1] =  arr[i+j+1]%10 ;
+                arr[i+j+1] += (num1.charAt(i)-48) * (num2.charAt(j)-48)  ;
+                arr[i+j]    +=  arr[i+j+1] / 10 ;
+                System.out.println(arr[i+j] + " -> " + arr[i+j+1]) ;
+                arr[i+j+1]  =  arr[i+j+1] % 10 ;
             }
         }
 
         String ans = "" ;
         boolean flag = true ;
 
-         for(int x : arr)
-         {
-             if(x == 0 && flag == true)
-             {
+        for(int i=0 ; i<arr.length ; i++)
+        {
+              if(arr[0] == 0 && flag == true)
+              {
+                flag = false ;
                 continue ;
-             }
-             else
-             {
-                 ans += x +"" ;
-                 flag = false ;
-             }
-         }
-          
-          return ans ;
-        
+              }
+
+            ans += arr[i] + "" ;
+        }
+        return ans ;
     }
 }
