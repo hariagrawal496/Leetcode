3a9920b27197299185dc2963f57374c4699cb88d
hariagrawal496 <hariagrawal.496@gmail.com>
2024-07-30 18:14:19 +0530

    Time: 17 ms (10.48%), Space: 45.4 MB (5.41%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -1,27 +1,25 @@
 class Solution {
     public String reverseWords(String s) 
     {
-         String[] arr = s.split(" ") ;
         String ans = "" ;
         String local = "" ;
 
-        for(String str : arr)
-        {
-            local = "" ;
-            for(char c : str.toCharArray())
+            for(char c : s.toCharArray())
             {
                 if(c != ' ')
                 {
                    local += c + "" ;
                 }
+                else
+                {
+                    if(local.length() > 0)
+                    {
+                        ans  = " " + local + ans ;
+                    }
+                    local = "";
+                }
             }
-
-            if(local.length() > 0)
-            {
-                ans  = " " + local + ans ;
-            }
-            
-        }
+            ans  = " " + local + ans ;
       return ans.trim() ;
     }
 }
