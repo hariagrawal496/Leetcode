bce61de1d710f464dd6d8c64e1c4643f9402d1cd
hariagrawal496 <hariagrawal.496@gmail.com>
2024-08-21 11:41:44 +0530

    Time: 1 ms (79.77%), Space: 41.8 MB (49.62%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.java
+++ 0091-decode-ways/0091-decode-ways.java
@@ -1,27 +1,30 @@
 class Solution {
     public int numDecodings(String s) 
     {
-        int n = s.length() ;
-        int[] dp = new int[n+1] ;
+      int n = s.length() ;
+      int[] dp = new int[n+1] ;
 
-        dp[0] = 1 ; // emtpy
-        dp[1] = s.charAt(0) =='0' ? 0 : 1 ; // length-->1
+      dp[0] = 1 ; // for empty
+      dp[1] = s.charAt(0) == '0' ? 0 : 1 ; // only for 1 element
 
-        for(int i=2; i<=n; i++)
+      // value  '' 1 1 1 0 6
+      // index   0 1 2 3 4 5  
+
+      for(int i=2; i<=n; i++) 
+      {
+        int oneDigit = Integer.parseInt(s.substring(i-1 , i)) ;
+        int twoDigit = Integer.parseInt(s.substring(i-2 , i)) ;
+
+        if(oneDigit > 0)
         {
-              int oneDigit =  Integer.parseInt(s.substring(i-1 , i)) ;
-              int twoDigit =  Integer.parseInt(s.substring(i-2 , i)) ;
-
-              if(oneDigit > 0)
-              {
-                dp[i] += dp[i-1] ;
-              }
-
-              if(twoDigit > 9 && twoDigit <= 26)
-              {
-                dp[i] += dp[i-2] ;
-              }
+          dp[i] = dp[i-1] ;
         }
-        return dp[n] ;
+
+        if(twoDigit > 9 && twoDigit < 27 )
+        {
+            dp[i] += dp[i-2] ;
+        }
+      }
+    return dp[n] ;
     }
 }
