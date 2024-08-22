78f4f8fa8fad9c09449bc38fa6c7fb294832ac9f
hariagrawal496 <hariagrawal.496@gmail.com>
2024-08-22 13:33:57 +0530

    Time: 1 ms (99.83%), Space: 46.5 MB (5.67%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,11 +1,13 @@
 class Solution {
     public int singleNumber(int[] nums) 
     {
-        int ans  = 0 ;
+        int ans = 0 ;
+
         for(int x : nums)
         {
             ans ^= x ;
         }
+        
         return ans ;
     }
 }
