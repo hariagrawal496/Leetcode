b8861bf16d6f14b3281cd51d0afe162ddcefd729
hariagrawal496 <hariagrawal.496@gmail.com>
2024-08-22 13:25:57 +0530

    Time: 9 ms (6.54%), Space: 44.9 MB (5.65%) - LeetHub

--- 0067-add-binary/0067-add-binary.java
+++ 0067-add-binary/0067-add-binary.java
@@ -1,40 +1,49 @@
 class Solution {
     public String addBinary(String a, String b) 
     {
-        int n = Math.max(a.length() , b.length()) ;
-        int[] arr = new int[n+1] ;
+        int len = Math.max(a.length() , b.length()) ;
+        int[] arr = new int[len+1] ;
 
-        int sum = 0 ;
-
-        for(int i=0; i<n; i++)
+        for(int i=0; i<len ; i++)
         {
-            sum = 0  ;
-            if(i < a.length())
+            int sum = 0 ;
+            if( i < a.length())
             {
-               sum = a.charAt(a.length()-1-i) - '0' ;
+              sum += a.charAt(a.length()-1 - i) - '0' ;
             }
 
             if( i < b.length())
             {
-                sum += b.charAt(b.length()-1-i) - '0' ;
+              sum += b.charAt(b.length()-1 - i) - '0' ;
             }
-            arr[n-i]  += sum ;
-            arr[n-1-i] +=  arr[n-i] / 2 ;
-            arr[n-i] =  arr[n-i]%2 ;
+
+            arr[len-i] += sum ;
+            arr[len-1-i] += arr[len-i]/2 ;
+            arr[len-i] = arr[len-i]%2 ;
         }
 
-        String ans  = "" ;
+        String ans = "" ;
         boolean flag = true ;
 
         for(int i=0; i<arr.length; i++)
         {
-            if(arr[0] == 0 && flag == true)
+            if(arr[0] == 0 &&flag == true)
             {
-               flag = false ;
-               continue ;
+                flag = false ;
+                continue ;
             }
-            ans += arr[i] + "" ;
+            else
+            {
+                ans += arr[i] + "" ;
+            }
         }
-        return ans ;
+        System.out.println(ans) ;
+
+        if(ans == "")
+        {
+            return "0" ;
+        }
+
+     return ans ;
     }
 }
