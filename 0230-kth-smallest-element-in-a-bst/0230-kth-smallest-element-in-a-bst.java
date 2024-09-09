b1fc15270f51ac9f869dee29de4b1834fe3cd3f6
hariagrawal496 <hariagrawal.496@gmail.com>
2024-09-09 09:20:52 +0530

    Time: 1 ms (35.15%), Space: 43.9 MB (97.58%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
@@ -22,19 +22,20 @@ class Solution {
         }
 
         List<Integer> list = new ArrayList<>() ;
-        inOrder(root , list) ;
+        inOrder(root , list) ;     
         return list.get(k-1) ;
     }
 
     public void inOrder(TreeNode root , List<Integer> list)
-    {   
+    {
         if(root == null)
         {
-            return  ;
+           return ;
         }
 
-        inOrder(root.left, list) ;
+        inOrder(root.left , list) ;
         list.add(root.val) ;
-        inOrder(root.right, list) ;
+        inOrder(root.right , list) ;
+        
     }
 }
