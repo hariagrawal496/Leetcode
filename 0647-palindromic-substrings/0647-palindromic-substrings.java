0da0601bf8f3f0bbb953dad02778480ecd635621
hariagrawal496 <hariagrawal.496@gmail.com>
2024-09-12 10:15:16 +0530

    Time: 6 ms (74.47%), Space: 41.5 MB (65.78%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.java
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -1,43 +1,42 @@
 class Solution {
     public int countSubstrings(String s) 
     {
-        if(s.length() == 0)
+        int n = s.length() ;
+        if(n == 0)
         {
             return 0 ;
         }
 
-        if(s.length() == 1)
+        if(n == 1)
         {
             return 1 ;
         }
 
         int count = 0 ;
-
-        for(int i=0; i<s.length(); i++)
+        for(int i=0; i<n; i++)
         {
-            count += checkPalindromeCount(s, i, i);
-            count += checkPalindromeCount(s, i, i+1);
+            count += checkpalindrome(i, i, s) ;
+            count += checkpalindrome(i, i+1, s) ;   
         }
-
-        return count ;
+         return count ;
     }
 
-    public int checkPalindromeCount(String str , int s , int e)
+    public int checkpalindrome(int s, int e, String str)
     {
         int count = 0 ;
-          while( s >= 0 && e < str.length())
-          {
-              if(str.charAt(s) == str.charAt(e))
-              {
+        while(s >= 0 && e < str.length())
+        {
+            if(str.charAt(s) == str.charAt(e))
+            {
                 count++ ;
-                   s-- ;
-                   e++ ;
-              }
-              else
-              {
+                s-- ;
+                e++ ;
+            }
+            else
+            {
                 break ;
-              }
-          }
-          return count ;
+            }
+        }
+       return count ;
     }
 }
