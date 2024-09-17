52fc5c81848ca3a5a1504e34d5ec839e9c09d0b6
hariagrawal496 <hariagrawal.496@gmail.com>
2024-09-17 16:10:11 +0530

    Time: 7 ms (71.53%), Space: 44.2 MB (34.18%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -4,44 +4,45 @@ class Solution {
         int sum = 0 ;
         for(int x : nums)
         {
-            sum += x ;
+          sum += x ;
         }
-          sum = sum + target ;
 
-          if(sum % 2 == 1 || sum < 0)
-          {
+        sum += target ;
+        if(sum % 2 == 1 || sum < 0)
+        {
             return 0 ;
-          }
+        }
 
-          int k = sum/2 ;
-          return countSubsetWithSumK(nums, k) ;
+        sum = sum/2 ;
+
+        return countNumberOfSubsetWithSumK(sum , nums) ;
     }
 
-    public int countSubsetWithSumK(int[] arr, int k)
+    public int countNumberOfSubsetWithSumK(int k , int[] nums)
     {
-        int[][] dp = new int[arr.length + 1][k + 1] ;
+        int[][] dp = new int[nums.length + 1][k + 1] ;
 
-        for(int i=0; i<=arr.length; i++)
+        for(int i=0; i<=nums.length; i++)
         {
             for(int j=0; j<=k; j++)
             {
-                   if( i==0 && j==0)
-                   {
+                if(i == 0  && j == 0)
+                {
                     dp[i][j] = 1 ;
                     continue ;
-                   }
+                }
 
-                   if(i > 0)
-                   {
-                       dp[i][j] += dp[i-1][j] ;
+                if( i > 0)
+                {
+                    dp[i][j] += dp[i-1][j] ;
 
-                       if(j-arr[i-1] >= 0)
-                       {
-                          dp[i][j] += dp[i-1][j-arr[i-1]] ;
-                       }
-                   }
+                    if( j-nums[i-1] >= 0)
+                    {
+                        dp[i][j] += dp[i-1][j-nums[i-1]] ;
+                    }
+                }
             }
         }
-        return dp[arr.length][k];
+       return dp[nums.length][k] ;
     }
 }
