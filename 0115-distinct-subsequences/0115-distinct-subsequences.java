e30c96af0764d8fb6fa882c729c217c5f8820ca8
hariagrawal496 <hariagrawal.496@gmail.com>
2024-09-18 11:06:09 +0530

    Time: 17 ms (16.46%), Space: 49.1 MB (27.8%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -21,14 +21,12 @@ class Solution {
                     continue ;
                 }
 
-                  if(s.charAt(i-1) == t.charAt(j-1))
-                  {
-                    dp[i][j] = dp[i-1][j-1] + dp[i-1][j] ;
-                  }
-                  else
-                  {
-                    dp[i][j] =  dp[i-1][j] ;
-                  }
+                dp[i][j] +=  dp[i-1][j] ;
+                
+                if(s.charAt(i-1) == t.charAt(j-1))
+                {
+                    dp[i][j] += dp[i-1][j-1] ;
+                }
             }
         }
       return dp[m][n] ;
