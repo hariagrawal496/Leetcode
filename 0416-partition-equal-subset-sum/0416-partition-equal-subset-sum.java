7a397aa7fe3e4f8b0ff8cd6a8e46f47356125e4b
hariagrawal496 <hariagrawal.496@gmail.com>
2024-09-23 17:30:37 +0530

    Time: 54 ms (43.66%), Space: 46.8 MB (51.38%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,23 +1,23 @@
 class Solution {
     public boolean canPartition(int[] nums) 
     {
-        if(nums.length == 0)
+        if(nums.length <= 1)
         {
-            return true ;
+            return false ;
         }
-  
-         int sum = 0 ;
-        for(int x: nums)
+
+        int sum = 0 ;
+        for(int x : nums)
         {
             sum += x ;
         }
 
-        if( sum % 2 == 1)
+        if(sum % 2 == 1 || sum < 0)
         {
             return false ;
         }
-        sum = sum/2 ;
 
+        sum = sum/2 ;
         for(int x : nums)
         {
             if(x > sum)
@@ -25,41 +25,40 @@ class Solution {
                 return false ;
             }
         }
-
-        return subSetwithSumK(nums, sum) ;
+        return checksubsetWithSumK(nums, sum) ;
     }
 
-    public boolean subSetwithSumK(int[] nums, int k)
+    public boolean checksubsetWithSumK(int[] nums, int k)
     {
-       boolean[][] dp = new boolean[nums.length + 1][k + 1] ;
+        boolean[][] dp = new boolean[nums.length + 1][k + 1] ;
 
-       for(int i=0; i<=nums.length; i++)
-       {
-        for(int j=0; j<=k; j++)
+        for(int i=0; i<=nums.length; i++)
         {
-             if( i == 0 && j == 0)
-             {
-                dp[i][j] = true ;
-                continue ;
-             }
-
-             if( i > 0)
-             {
-                  if(dp[i-1][j] == true)
-                  {
-                     dp[i][j] = true ;
-                  }
-                  else if( j >= nums[i-1] && dp[i-1][j-nums[i-1]] == true)
-                  {
+            for(int j=0; j<=k ; j++)
+            {
+                if( i == 0 && j == 0)
+                {
                     dp[i][j] = true ;
-                  }
-                  else
-                  {
-                    dp[i][j] = false ;
-                  }
-             }
+                    continue ;
+                }
+
+                if( i > 0)
+                {
+                    if(dp[i-1][j] == true)
+                    {
+                        dp[i][j] = true ;
+                    }
+                    else if((j - nums[i-1]) >= 0 && (dp[i-1][j - nums[i-1]]) == true)
+                    {
+                       dp[i][j] = true ;
+                    }
+                    else
+                    {
+                        dp[i][j] = false ;
+                    }
+                }
+            }
         }
-       }
-     return dp[nums.length][k] ;
+        return dp[nums.length][k] ;
     }
 }
