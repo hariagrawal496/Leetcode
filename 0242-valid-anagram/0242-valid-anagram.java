492d2df05b90e1c4253def55dc4cace5e10f4bc9
hariagrawal496 <hariagrawal.496@gmail.com>
2024-12-17 08:56:26 +0530

    Time: 4 ms (76.62%), Space: 44.7 MB (25.89%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -12,9 +12,13 @@ class Solution {
         Arrays.sort(arr1) ;
         Arrays.sort(arr2) ;
 
-        String str1 = new String(arr1) ;
-        String str2 = new String(arr2) ;
-
-        return str1.equals(str2) ;
+        for(int i=0; i<arr1.length ; i++)
+        {
+            if(arr1[i] != arr2[i])
+            {
+               return false ;
+            }
+        }
+    return true ;
     }
 }
