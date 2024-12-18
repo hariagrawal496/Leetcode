35f748fc63353b6a5df1cabab79a74859952369d
hariagrawal496 <hariagrawal.496@gmail.com>
2024-12-18 08:50:22 +0530

    Time: 2 ms (97.48%), Space: 41.7 MB (47.17%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,45 +1,50 @@
 class Solution {
     public boolean isValid(String s) 
     {
+        if(s == "")
+        {
+            return true ;
+        }
+
+        if(s.length() % 2 != 0)
+        {
+            return false ;
+        }
+
         Stack<Character> stack = new Stack<>() ;
 
-          for(char c : s.toCharArray())
-          {
-              if(stack.isEmpty())
-              {
-                 stack.push(c) ;
-                 continue ;
-              }
-
-              if(c == ')' || c == ']' || c == '}')
-              {
-                  if(c == '}' && stack.peek() == '{')
-                  {
-                    stack.pop() ;
-                  }
-                  else if(c == ')' && stack.peek() == '(')
-                  {
-                    stack.pop() ;
-                  }
-                  else if(c == ']' && stack.peek() == '[')
-                  {
-                    stack.pop() ;
-                  }
-                  else
-                  {
-                    return false ;
-                  }
-              }
-              else
-              {
+        for(char c : s.toCharArray())
+        {
+            if(stack.isEmpty())
+            {
                 stack.push(c) ;
-              }
-          }
-
-          if(stack.isEmpty())
-          {
-             return true ;
-          }
-          return false ;
+                continue ;
+            }
+            
+            if(c == '}' || c == ']' || c == ')')
+            {
+                if(c == ')' && stack.peek() == '(')
+                {
+                    stack.pop() ;
+                }
+                else if(c == ']' && stack.peek() == '[')
+                {
+                    stack.pop() ;
+                }
+                else if(c == '}' && stack.peek() == '{')
+                {
+                    stack.pop() ;
+                }
+                else
+                {
+                    return false ;
+                }
+            }
+            else
+            {
+                stack.push(c) ;
+            }
+        }
+    return stack.isEmpty() ;
     }
 }
