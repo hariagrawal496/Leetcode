407ecaba88eb402f767734a229da236bd4f8ef23
hariagrawal496 <hariagrawal.496@gmail.com>
2025-01-01 16:36:10 +0530

    Time: 0 ms (100%), Space: 42.2 MB (22.05%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,22 +1,6 @@
 class Solution {
     public void sortColors(int[] nums) 
     {
-        HashMap<Integer ,Integer> map = new HashMap<>() ;
-        for(int x : nums)
-        {
-            map.put(x, map.getOrDefault(x,0)+1) ;
-        }
-
-        int j=0; 
-
-        for(int x : map.keySet())
-        {
-            int count = map.get(x) ;
-
-            while(count-- != 0)
-            {
-                nums[j++] = x ;
-            }
-        }
+        Arrays.sort(nums) ;
     }
 }
