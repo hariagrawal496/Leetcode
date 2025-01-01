a46fe4aae2fb08e563077e06de872c48cce76ec8
hariagrawal496 <hariagrawal.496@gmail.com>
2025-01-01 16:39:52 +0530

    Time: 0 ms (100%), Space: 41.8 MB (80.26%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,6 +1,41 @@
 class Solution {
     public void sortColors(int[] nums) 
     {
-        Arrays.sort(nums) ;
+        int count_0 = 0 ;
+        int count_1 = 0 ;
+        int count_2 = 0 ;
+
+        for(int x : nums)
+        {
+            if(x == 1)
+            {
+                count_1++ ;
+            }
+            else if( x == 0)
+            {
+                count_0++ ;
+            }
+            else
+            {
+                count_2++ ;
+            }
+        }
+
+        int i=0; 
+        while(count_0-- != 0)
+        {
+            nums[i++] = 0 ;
+        }
+
+        while(count_1-- != 0)
+        {
+            nums[i++] = 1 ;
+        }
+
+        while(count_2-- != 0)
+        {
+            nums[i++] = 2 ;
+        }
+
     }
 }
