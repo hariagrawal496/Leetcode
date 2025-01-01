03c14c075c04b09672ff6c69e207e517bbeb143b
hariagrawal496 <hariagrawal.496@gmail.com>
2025-01-01 16:30:29 +0530

    Time: 1 ms (21.85%), Space: 42 MB (50.94%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,17 +1,17 @@
 class Solution {
     public void sortColors(int[] nums) 
     {
-        PriorityQueue<Integer> minHeap = new PriorityQueue<>() ;
-
-        for(int x : nums)
-        {
-            minHeap.add(x) ;
-        }
-
         for(int i=0; i<nums.length; i++)
         {
-            nums[i] = minHeap.remove() ;
-        }
-        
+            for(int j=i+1; j<nums.length; j++)
+            {
+                if(nums[i] > nums[j])
+                {
+                    int temp = nums[i] ;
+                    nums[i] = nums[j] ;
+                    nums[j] = temp ;
+                }
+            }
+        }      
     }
 }
