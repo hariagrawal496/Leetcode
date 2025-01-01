47620efcffcc91e8753638682e7ba0f4350fba33
hariagrawal496 <hariagrawal.496@gmail.com>
2025-01-01 19:07:07 +0530

    Time: 10 ms (11.5%), Space: 44.4 MB (13.43%) - LeetHub

--- 0179-largest-number/0179-largest-number.java
+++ 0179-largest-number/0179-largest-number.java
@@ -2,33 +2,23 @@ class Solution {
     public String largestNumber(int[] nums) 
     {
         int n = nums.length ;
-        String[] arr = new String[n] ;
+        List<String> list = new ArrayList<>() ;
           
         for(int i=0; i<n; i++)
         {
-            arr[i] = String.valueOf(nums[i]) ;
+            String val = String.valueOf(nums[i]) ;
+            list.add(val) ;
         }
 
-        Comparator<String> comp = new Comparator<String>()
-        {
-            @Override
-            public int compare(String s1 , String s2)
-            {
-                String str1 = s1 + s2 ;
-                String str2 = s2 + s1 ;
-
-                return str2.compareTo(str1) ;
-            }
-        } ;
-
-        Arrays.sort(arr, comp) ;
-        if(arr[0].charAt(0) == '0')
+       list = list.stream().sorted((a,b) -> (b+a).compareTo(a+b)).toList() ;
+         
+        if(list.get(0).charAt(0) == '0')
         {
             return "0" ;
         }
 
         String ans = "" ;
-        for(String s : arr)
+        for(String s : list)
         {
             ans += s + "" ;
         }
