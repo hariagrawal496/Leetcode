d1e67c9f8e2541073123b380469a55f1c2c96b23
hariagrawal496 <hariagrawal.496@gmail.com>
2025-01-03 17:25:13 +0530

    Time: 25 ms (8.09%), Space: 44.8 MB (5.22%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -14,8 +14,29 @@ class Solution {
             char s1 = s.charAt(i) ;
             char t1 = t.charAt(i) ;
 
-            int indexS = map1.getOrDefault(s1, -1) ;
-            int indexT = map2.getOrDefault(t1, -1) ;
+            int indexS = 0 ;
+            int indexT = 0 ;
+
+            if(map1.containsKey(s1))
+            {
+               indexS = map1.get(s1) ;
+            }
+            else
+            {
+                indexS = -1 ;
+            }
+
+            if(map2.containsKey(t1))
+            {
+               indexT = map2.get(t1) ;
+            }
+            else
+            {
+                indexT = -1 ;
+            }
+
+           // int indexS = map1.getOrDefault(s1, -1) ;
+          //  int indexT = map2.getOrDefault(t1, -1) ;
 
             if(indexS != indexT)
             {
