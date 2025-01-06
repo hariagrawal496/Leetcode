98350010e4f8b4a3582f765850463d036acc55b3
hariagrawal496 <hariagrawal.496@gmail.com>
2025-01-06 09:38:26 +0530

    Time: 1 ms (81.44%), Space: 42.1 MB (20.96%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.java
+++ 0091-decode-ways/0091-decode-ways.java
@@ -1,26 +1,30 @@
 class Solution {
     public int numDecodings(String s) 
     {
-        int[] dp = new int[s.length() + 1] ;
+        int n = s.length() ;
+        int[] dp = new int[n+1] ;
+        
         dp[0] = 1 ;
-        dp[1] = (s.charAt(0)-'0' > 0) ? 1 : 0  ;
-
-        for(int i=2; i<=s.length() ; i++)
+        dp[1] = (s.charAt(0) == '0') ? 0 : 1 ; // first digit
+      
+        for(int i=2; i<=n; i++)
         {
-            int oneDigit = Integer.parseInt(s.substring(i-1,i));
-            int twoDigit = Integer.parseInt(s.substring(i-2,i));
+            String oneDigit = s.substring(i-1, i) ;
+            String twoDigit = s.substring(i-2, i) ;
 
-            if(oneDigit > 0 )
+            int one = Integer.parseInt(oneDigit) ;
+            int two = Integer.parseInt(twoDigit) ;
+
+            if(one != 0)
             {
-                dp[i] += dp[i-1] ;
+                dp[i]  = dp[i-1] ;
             }
 
-            if(twoDigit > 9 && twoDigit < 27 )
+            if(two > 9 && two <= 26)
             {
-                dp[i] += dp[i-2] ;
+                dp[i]  += dp[i-2] ;
             }
-           
         }
-        return dp[s.length()];
+    return dp[n] ;
     }
 }
