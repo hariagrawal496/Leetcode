df2e49a71d8fed2f994bd25d5b99d64c83cade8e
hariagrawal496 <hariagrawal.496@gmail.com>
2025-01-09 13:49:50 +0530

    Time: 0 ms (100%), Space: 41 MB (61.94%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -21,11 +21,11 @@ class Solution {
             return true ;
         }
 
-        if(p == null && q != null || p != null && q == null || p.val != q.val)
+        if((p != null && q == null) || (p == null && q != null) || (p.val != q.val )  )
         {
             return false ;
-        } 
-
+        }
+        
         return isSameTree(p.left, q.left) && isSameTree(p.right, q.right) ;
     }
 }
