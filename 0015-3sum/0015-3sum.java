9b5c20512d899a32e61ee6b8c562ddb1da4ef7a2
hariagrawal496 <hariagrawal.496@gmail.com>
2025-02-01 09:59:33 +0530

    Time: 380 ms (22.06%), Space: 52.9 MB (13.15%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,39 +1,42 @@
 class Solution {
-    public List<List<Integer>> threeSum(int[] nums) 
+    public List<List<Integer>> threeSum(int[] A) 
     {
-        Arrays.sort(nums) ;
-        int target = 0 ;
-        Set<List<Integer>> set = new HashSet<>() ;
-
-        for(int i=0; i<nums.length; i++)
-        {
-            int s= i+1;
-            int e = nums.length -1 ;
-
-            while( s < e)
-            {
-                if( nums[i] + nums[s] + nums[e] == target)
-                {
-                    List<Integer> list = new ArrayList<>() ;
-                     list.add(nums[i]) ;
-                     list.add(nums[s]) ;
-                     list.add(nums[e]) ;
-
-                     set.add(list) ;
-
-                     s++ ;
-                     e-- ;
-                }
-                else if( nums[i] + nums[s] + nums[e] > target)
-                {
-                    e-- ;
-                }
-                else
-                {
-                    s++ ;
-                }
-            }
-        }
-         return new ArrayList<>(set) ;
+        int n = A.length  ;
+		Set<List<Integer>> set = new HashSet<>() ;
+		
+	    Arrays.sort(A) ;
+		
+		for(int i=0; i<n; i++)
+		{
+			int s = i+1 ;
+			int e = n-1 ;
+				
+			while(s < e)
+			{
+				long sum = (long)(A[i] + A[s] + A[e])	 ;
+					
+					if(sum == 0)
+					{
+						List<Integer> data = new ArrayList<>() ;		 
+							data.add(A[i]) ;
+							data.add(A[s]) ;
+							data.add(A[e]) ;						
+						set.add(data) ;			
+						
+						s++ ;
+						e-- ;
+					}
+				  	else if(sum > 0)
+				    {
+					   	e-- ;
+				    }
+					else
+					{
+						s++ ;
+				 	}
+			}
+		}
+		return new ArrayList<>(set) ;
+        
     }
 }
