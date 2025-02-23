5a316cb75cfe108672c8f5269d998eaa3d3bc86c
hariagrawal496 <hariagrawal.496@gmail.com>
2025-02-23 20:11:27 +0530

    Time: 0 ms (100%), Space: 46.1 MB (46.5%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,68 +1,52 @@
 class Solution {
-    public int[] searchRange(int[] arr, int key) 
+    public int[] searchRange(int[] nums, int target) 
     {
-    	int[] data = new int[2] ;	
-		data[0] = firstOccurance(arr, key) ;
-		data[1] = LastOccurance(arr, key) ;
-		
-		return data ;
-	}
-	
-	public int firstOccurance(int[] arr, int key)
-	{
-		int s = 0 ;
-		int e = arr.length-1 ;
-		
-		int first = -1 ;
-		
-		while(s <= e && e < arr.length)
-		{
-			int mid = s + (e-s)/2 ;
-			
-			if(arr[mid] == key)
-			{
-				first = mid ;
-				 e = mid-1 ;
-			}	
-			else if(arr[mid] < key)
-			{
-				s = mid + 1 ;
-			}
-			else
-			{
-				e = mid - 1 ;
-			}
-			
-		}
-		return first ;
-	}
-	
-	public int LastOccurance(int[] arr, int key)
-	{
-		int s = 0 ;
-		int e = arr.length-1 ;
-		
-		int last = -1 ;
-		
-		while(s <= e && e < arr.length)
-		{
-			int mid = s + (e-s)/2 ;
-			
-			if(arr[mid] == key)
-			{
-				last = mid ;
-				 s = mid + 1 ;
-			}	
-			else if(arr[mid] > key)
-			{
-				e = mid - 1 ;
-			}
-			else
-			{
-				s = mid + 1 ;
-			}
-			
-		}
-		return last ;
-	}
+        int s = 0 ;
+        int e = nums.length-1 ;
+        int lastElement = -1 ;
+
+         while(s <= e)
+         {
+            int mid = s + (e-s)/2 ;
+
+            if(nums[mid] == target)
+            {
+                lastElement = mid ;
+            }
+
+              if(nums[mid] > target)
+              {
+               e = mid - 1;  
+              }
+              else
+              {
+                s = mid + 1 ;
+              }
+         }
+
+        s = 0 ;
+        e = nums.length-1 ;
+        int firstElement = -1 ;
+
+        while(s <= e)
+        {
+            int mid = s + (e-s)/2 ;
+
+            if(nums[mid] == target)
+            {
+                firstElement = mid ;
+            }
+
+              if(nums[mid] < target)
+              {
+                  s =  mid + 1 ;
+              }
+              else
+              {
+                  e = mid - 1 ; 
+              }
+        }
+
+         return new int[]{firstElement, lastElement} ;
+    }
 }
