966d3e687791bc15942c50edd65d47938d2bafa0
hariagrawal496 <hariagrawal.496@gmail.com>
2025-02-24 21:52:44 +0530

    Time: 24 ms (5.19%), Space: 48.6 MB (67.84%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -7,30 +7,35 @@ class Solution {
             map.put(x, map.getOrDefault(x, 0)+1) ;
         }
 
-        List<Integer>[] bucket = new ArrayList[nums.length + 1 ] ;
+        Map< Integer, List<Integer> > insideMap = new TreeMap<>((a,b) -> b-a) ;
         for(int x : map.keySet())
         {
-            int val = map.get(x) ;
-            if(bucket[val] == null)
-            {
-                bucket[val] =  new ArrayList<>() ;
-            }
-            bucket[val].add(x) ;
+             int val = map.get(x) ;    
+             if(!insideMap.containsKey(val))
+             {
+                insideMap.put(val, new ArrayList<>()) ;
+             }
+
+             insideMap.get(val).add(x) ;
         }
 
-        int[] ans = new int[k] ;
-        int count = 0  ;
+        int[] arr = new int[k] ;
+        int j = 0 ;
 
-        for(int i=bucket.length-1; i>=0; i--)
+        for(int x : insideMap.keySet() )
         {
-           if(bucket[i] != null)
+           List<Integer> list = insideMap.get(x) ;
+
+           System.out.println(x + " -> " + list) ;
+
+           for(int i=0; i<list.size() ; i++)
            {
-              for(int j=0; j<bucket[i].size() && count < k ; j++)
-              {
-                  ans[count++] = bucket[i].get(j) ;
-              }
-           }
+                if( j < k)
+                {
+                    arr[j++] = list.get(i) ;
+                }          
+           }      
         }
-     return ans ;
+       return arr ;
     }
 }
