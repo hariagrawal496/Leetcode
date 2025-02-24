b110e47b2d74210b736e98980585831e993b2656
hariagrawal496 <hariagrawal.496@gmail.com>
2025-02-24 14:43:27 +0530

    Time: 47 ms (9.58%), Space: 45.9 MB (71.46%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
@@ -3,17 +3,33 @@ class Solution {
     {
         int m = matrix.length ;
         int n = matrix[0].length ;
+        boolean flag = false ;
+
+        int[] arr = new int[m*n] ;
+        int x = 0 ;
 
         for(int i=0; i<m; i++)
         {
             for(int j=0; j<n; j++)
             {
-                   if(matrix[i][j] == target)
-                   {
-                    return true ;
-                   }
+                arr[x++] = matrix[i][j] ;
+                if(matrix[i][j] == target)
+                {
+                    flag = true ;
+                }
             }
         }
-    return false ;
+
+         x = 0 ;
+         Arrays.sort(arr) ;
+
+        for(int i=0; i<m; i++)
+        {
+            for(int j=0; j<n; j++)
+            {
+                 matrix[i][j]  = arr[x++];
+            }
+        }      
+    return flag ;
     }
 }
