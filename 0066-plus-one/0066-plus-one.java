dfda1bb58d19762a4514cdfe5cc6a2e7fd829aea
hariagrawal496 <hariagrawal.496@gmail.com>
2025-03-07 23:51:48 +0530

    Time: 0 ms (100%), Space: 41.9 MB (46.09%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,37 +1,38 @@
 class Solution {
     public int[] plusOne(int[] digits) 
     {
-        if(digits.length == 0)
-        {
-            return null ;
-        }
-        
-        int n = digits.length;
+         int n = digits.length ;
         int[] arr = new int[n+1] ;
-        int[] new_arr = new int[n] ;
-        int value = 0 ;
-      
-        for(int i=n-1; i>=0; i--)
+    
+        for(int i=n-1; i>=0; i--) // 99
         {
-           if( i == n-1)  
-           {
-             value = digits[i] + 1 ;  
-           }
-           else
-           {
-            value = digits[i]  ; 
-           }
+            if( i == n-1)
+            {
+                int val = digits[i] + 1 ;
+                arr[i] = val / 10 ;
+                arr[i+1] = val % 10 ;
+            } 
+            else
+            {
+                arr[i+1] += digits[i] ;
+                arr[i] += arr[i+1] / 10 ;
+                arr[i+1] = arr[i+1] % 10 ;
 
-             arr[i+1]  += value  ; 
-               arr[i]   = arr[i+1] / 10 ;
-             arr[i+1]   = arr[i+1] % 10 ;   
-           new_arr[i]   = arr[i+1] ;     
+            }      
         }
+
+        int[] arr1 = new int[n] ;
 
         if(arr[0] == 0)
         {
-            return new_arr ;
+            for(int i=1; i<=n; i++)
+            {
+                arr1[i-1] = arr[i] ;
+            }
+
+            return arr1 ;
         }
-      return arr ;
+
+       return arr ;
     }
 }
