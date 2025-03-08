8e00ebc66c5eb2c5fd52123050f6a438119934eb
hariagrawal496 <hariagrawal.496@gmail.com>
2025-03-08 12:37:58 +0530

    Time: 5 ms (54.57%), Space: 45 MB (24.02%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,34 +1,31 @@
 class Solution {
     public int romanToInt(String s) 
     {
-        Map<Character,Integer> map = new HashMap<>();
-        map.put('I' , 1) ;
-        map.put('V' , 5) ;
-        map.put('X' , 10) ;
-        map.put('L' , 50) ;
-        map.put('C' , 100) ;
-        map.put('D' , 500) ;
-        map.put('M' , 1000) ;
-
         int n = s.length() ;
-        int ans = map.get(s.charAt(n-1)) ;
-        int prev =0 ;
-        int curr = 0 ;
-    
-        for(int i=1; i<n ; i++) 
-        {
-            prev = map.get(s.charAt(i-1)) ;
-            curr = map.get(s.charAt(i)) ;
+        Map<Character,Integer> map = new HashMap<>() ;
+        map.put('I' , 1);
+        map.put('V' , 5);
+        map.put('X' , 10);
+        map.put('L' , 50);
+        map.put('C' , 100);
+        map.put('D' , 500);
+        map.put('M' , 1000);
 
-           if(prev < curr)
-           {
-             ans -= prev ;
-           }
-           else
-           {
-              ans += prev ;
-           }
+
+        int ans = 0 ;
+
+        for(int i=1; i<n; i++)
+        {
+            if(map.get(s.charAt(i)) > map.get(s.charAt(i-1)) )
+            {
+                ans -= map.get(s.charAt(i-1)) ;
+            }
+            else
+            {
+                ans += map.get(s.charAt(i-1)) ;
+            }
         }
-        return Math.abs(ans) ;
+
+        return  ans +  map.get(s.charAt(n-1)) ;
     }
 }
