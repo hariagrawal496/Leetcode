74682b2e50bac316777df722f247be21a10ae954
hariagrawal496 <hariagrawal.496@gmail.com>
2025-04-07 22:53:11 +0530

    Time: 8 ms (3.74%), Space: 42.1 MB (5.93%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.java
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -1,47 +1,42 @@
 class Solution {
     public int reverse(int x) 
     {
-        long value = x ;
-        if(x < 0)
-        {
-            x = (-1) * x ;
-        }
-
         String str = String.valueOf(x) ;
-        int s = 0 ;
-        int e = str.length()-1 ;
         char[] arr = str.toCharArray() ;
 
-        while(s <= e && e < str.length())
+        int s = 0 ;
+        int e = str.length()-1 ;
+
+        while( s <= e)
         {
-            char temp = arr[s] ;
+            char ch = arr[s] ;
             arr[s] = arr[e] ;
-            arr[e] = temp ;
+            arr[e] = ch ;
 
             s++ ;
             e-- ;
         }
-        
-           str = "" ;
-          for(char c : arr)
-          {
-             if( c != '-')
+
+        String st = "" ;
+         for(char c : arr)
+         {
+             if(c != '-')
              {
-                str += c +"" ;
+                st += c + "" ;
              }
-          }
-        long ans = Long.parseLong(str) ;
-
-        if(value < 0)
-        {
-            ans *= -1 ;
-        }
-
-        if(ans > Integer.MAX_VALUE || ans < Integer.MIN_VALUE)
+         }
+    
+        long ans = Long.parseLong(st) ;
+        if(ans > Integer.MAX_VALUE || ans < Integer.MIN_VALUE )
         {
             return 0 ;
         }
 
-        return (int)ans ;
+        if(x < 0)
+        {
+            return (int)(-1 * ans) ;
+        }
+
+     return (int)ans ;
     }
 }
